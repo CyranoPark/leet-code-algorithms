bc7b0befcc769c03fd06e59ced99680a5c975957
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-07-10 14:36:42 +0900

    Update stone-game.js
    로직 수정

--- stone-game/stone-game.js
+++ stone-game/stone-game.js
@@ -3,28 +3,34 @@
  * @return {boolean}
  */
 var stoneGame = function(piles) {
-    
-    return getMaximum(piles, true) > (Math.max(...piles) / 2);
-    
-    function getMaximum(stones, isFirst) {
-        const n = stones.length;
-        const start = stones[0];
-        const end = stones[n - 1];
-        const pivot = isFirst ? start : 0;
-        
-        
-        if (n === 1) {
-            if (isFirst) {
-                return start;
-            } else {
-                return 0;
-            }
-            
-        }
-        
-        const startMax = pivot + getMaximum(stones.slice(1), !isFirst);
-        const endMax = pivot + getMaximum(stones.slice(-1), !isFirst);
-        
-        return Math.max(startMax, endMax);
-    }  
+
+  const minCounter = Math.min(
+    getMaximum(piles.slice(1), true),
+    getMaximum(piles.slice(0, -1), true)
+  );
+  const sum = piles.reduce((acc, number) => acc + number);
+  
+  return minCounter < sum / 2;
+
+  function getMaximum(stones, isFirst) {
+    const n = stones.length;
+    const start = stones[0];
+    const end = stones[n - 1];
+    const startPivot = isFirst ? start : 0;
+    const endPivot = isFirst ? end : 0;
+
+
+    if (n === 2) {
+      if (start > end) {
+        return isFirst ? start : end;
+      } else {
+        return isFirst ? end : start;
+      }
+    }
+
+    const startMax = startPivot + getMaximum(stones.slice(1), !isFirst);
+    const endMax = endPivot + getMaximum(stones.slice(0, -1), !isFirst);
+
+    return Math.max(startMax, endMax);
+  }
 };
