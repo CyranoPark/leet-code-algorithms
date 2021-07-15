3b77e4faebdc4bc09d0f5b6f0e56ba6cfe099773
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-07-15 19:28:40 +0900

    Update kth-missing-positive-number.js

--- kth-missing-positive-number/kth-missing-positive-number.js
+++ kth-missing-positive-number/kth-missing-positive-number.js
@@ -4,20 +4,29 @@
  * @return {number}
  */
 var findKthPositive = function(arr, k) {
-    let n = k;
-    for (let i = 0; i < arr.length; i++) {
-        const prevNumber = arr[i - 1] || 0;
-        const missingCount = arr[i] - prevNumber - 1;
-        
-        if (n <= missingCount) {
-            return prevNumber + n;
+    
+    return binarySearchKth(0, arr.length - 1);
+    
+    function binarySearchKth(from, to) {
+        if (from === to) {
+            const missingCount = getMissingCount(from);
+            
+            return arr[from] +
+                (k - missingCount) -
+                (missingCount >= k ? 1 : 0);
         }
         
-        if (n > missingCount) {
-            n -= missingCount;
-        }
+        const centerIndex = Math.floor((to - from) / 2) + from;
         
+        if (getMissingCount(centerIndex) >= k) {
+            return binarySearchKth(from, centerIndex);
+        } else {
+            return binarySearchKth(centerIndex + 1, to);
+        }
     }
     
-    return arr[arr.length - 1] + n;
+    function getMissingCount(i) {
+        return arr[i] - i - 1;
+    }
+    
 };
