888381eff497b53d4c452f7a9a0b319569f13958
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-07-17 10:03:42 +0900

    Time: 68 ms (92.09%), Space: 39.5 MB (25.12%) - LeetHub

---
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+  
+    return binarySearch(0, nums.length - 1)
+    
+    function binarySearch(from, to) {
+        const pivot = from + Math.floor((to - from) / 2);
+        
+        if (nums[pivot] !== nums[pivot + 1] && nums[pivot] !== nums[pivot - 1]) {
+            return nums[pivot];
+        }
+        
+        if (pivot % 2 === 0) {
+            if (nums[pivot] === nums[pivot + 1]) {
+                return binarySearch(pivot + 2, to);
+            } else {
+                return binarySearch(from, pivot);
+            }
+        } else {
+            if (nums[pivot] === nums[pivot + 1]) {
+                return binarySearch(from, pivot);
+            } else {
+                return binarySearch(pivot + 1, to);
+            }
+        }
+        
+    }
+};
