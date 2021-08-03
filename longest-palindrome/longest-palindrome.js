a6a9a33035e2bdf0cce1f46a244066526dda2e7f
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-08-03 21:21:57 +0900

    Update longest-palindrome.js

--- longest-palindrome/longest-palindrome.js
+++ longest-palindrome/longest-palindrome.js
@@ -4,17 +4,16 @@
  */
 var longestPalindrome = function(s) {
     const letters = {};
-    let count = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        letters[s[i]] = letters[s[i]] ? letters[s[i]] + 1 : 1;
+    const count = s.split('').reduce((acc, letter) => {
+        letters[letter] = letters[letter] ? letters[letter] + 1 : 1;
         
-        if (letters[s[i]] > 1) {
-            letters[s[i]] = 0;
-            count += 2;
+        if (letters[letter] > 1) {
+            letters[letter] = 0;
+            acc += 2;
         }
-    }
-    
+        
+        return acc;
+    }, 0);
     const hasSingle = Object.values(letters).some(v => v);
     
     return count + (hasSingle ? 1 : 0);
