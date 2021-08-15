bcff6809f7d154164ce7f02fe0d82627864669b7
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-08-15 12:42:07 +0900

    Time: 109 ms (7.07%), Space: 40.8 MB (54.55%) - LeetHub

---
+++ validate-stack-sequences/validate-stack-sequences.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} pushed
+ * @param {number[]} popped
+ * @return {boolean}
+ */
+var validateStackSequences = function(pushed, popped) {
+    let poppedIndex = 0;
+    const result = [];
+    
+    for (let i = 0; i < pushed.length; i++) {
+        result.push(pushed[i]);
+        
+        while (poppedIndex < popped.length && popped[poppedIndex] === result[result.length - 1]) {
+            poppedIndex++;
+            result.pop();
+        }
+    }
+    
+    console.log(result);
+    return !result.length;
+    
+};
