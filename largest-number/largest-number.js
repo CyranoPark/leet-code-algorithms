3bbd7fc68e7b74022629708991e441f2a4e5cdcb
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-08-22 11:37:05 +0900

    Time: 88 ms (25.57%), Space: 40.5 MB (60.23%) - LeetHub

---
+++ largest-number/largest-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    const sorted = [...nums].sort((a, b) => {
+        const aStr = a.toString();
+        const bStr = b.toString();
+        
+        return parseInt(bStr + aStr) - parseInt(aStr + bStr);
+        
+    });
+    
+    return removeZero(sorted).join('');
+
+    
+    function removeZero(nums) {
+        // if (nums.length < 2) {
+        //     return nums;
+        // }
+        
+        for (let i = 0; i < nums.length - 1; i++) {
+            if (nums[i] === 0) {
+                nums[i] = '';
+            } else {
+                return nums;
+            }
+        }
+        
+        return nums;
+    }
+    
+};
