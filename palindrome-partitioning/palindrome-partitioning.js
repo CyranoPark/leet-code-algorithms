d9489936c98545237cae71f192c62d329bdd398f
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2021-10-03 12:21:33 +0900

    Time: 785 ms (6.32%), Space: 94.8 MB (5.10%) - LeetHub

---
+++ palindrome-partitioning/palindrome-partitioning.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+var partition = function(s) {
+    const result = [];
+    let current = 0;
+    
+    fn(0, []);
+    
+    return result;
+
+    function fn (start, arr) {
+        if (start === s.length) {
+            result.push(arr);
+            return;
+        }
+        
+        for (let i = start; i < s.length; i++) {
+            const string = s.slice(start, i + 1);
+            const reversed = string.split('').reverse().join('');
+            console.log(string);
+            
+            if (string === reversed) {
+                const newArr = arr.slice();
+                newArr.push(string);
+                fn(i + 1, newArr);
+            }
+        }
+    }
+};
