383c00e19b7ca7f8eef8482e4b101c7dcde140f0
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2022-01-14 23:14:15 +0900

    Time: 144 ms (6.61%), Space: 40.1 MB (75.21%) - LeetHub

---
+++ 419-battleships-in-a-board/419-battleships-in-a-board.js
@@ -0,0 +1,36 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var countBattleships = function(board) {
+    let count = 0;
+    
+    board.forEach((row, i) => {
+        row.forEach((col, j) => {
+            if (col === 'X') {
+                search(i, j);
+                count++;
+            }
+            if (col === 'O') {
+                board[i][j] = 'X';
+            }
+        });
+    });
+    
+    function search(i, j) {
+        
+        board[i][j] = 'O';
+        
+        if (board[i][j + 1] === 'X') {
+            search(i, j + 1);
+            return;
+        }
+        
+        if (board[i + 1]?.[j] === 'X') {
+            search(i + 1, j);
+            return;
+        }
+    }
+    
+    return count;
+};
