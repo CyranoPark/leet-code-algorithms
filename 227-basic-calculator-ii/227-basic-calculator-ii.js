9cd75d87fe57e4b00e083cf83fdb76e4b5914a71
CyranoPark <48800862+CyranoPark@users.noreply.github.com>
2022-01-23 06:08:51 +0900

    Time: 156 ms (18.62%), Space: 43.6 MB (60.16%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    const stack = [];
+    
+    let lastOperator = '+';
+    let currentNumber = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        const integer = s[i];
+        
+        if (!isNaN(integer)) {
+            currentNumber = currentNumber + integer;
+            
+            if (i !== s.length - 1) continue;
+        }
+
+        if (lastOperator === '*') {
+            const lastInteger = stack.pop();
+            stack.push(lastInteger * Number(currentNumber));
+            currentNumber = '';
+        }
+        
+        if (lastOperator === '/') {
+            const lastInteger = stack.pop();
+            const divided = lastInteger / Number(currentNumber);
+            
+            stack.push(divided < 0 ? Math.ceil(divided) : Math.floor(divided));
+            currentNumber = '';
+        }
+
+        if (lastOperator === '+') {
+            stack.push(Number(currentNumber));
+            currentNumber = '';
+        }
+        if (lastOperator === '-') {
+            stack.push(Number(currentNumber) * -1);
+            currentNumber = '';
+        }
+        
+        lastOperator = integer;
+    }
+console.log(stack)
+    return stack.reduce((acc, num) => acc + num, 0);
+};
